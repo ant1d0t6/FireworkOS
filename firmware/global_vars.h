ccd0d738e2507667bc40da51e22f43cebae22786
ant1d0t6 <mascha281534@gmail.com>
2025-10-10 11:43:53 +0300

    Create global_vars.h

---
+++ firmware/global_vars.h
@@ -0,0 +1,35 @@
+/*
+
+FireworkOS 1.0 (Global vars)
+Copyright (C) 2025 hidely/ResiChat
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+// global_vars.h
+#ifndef GLOBAL_VARS_H
+#define GLOBAL_VARS_H
+
+#include <Arduino.h>
+
+extern boolean sd_initialized;
+extern boolean storage_mounted;
+extern boolean retrocat_inited;
+extern boolean has_wifi;
+extern String devicetype;
+extern String kernelver;
+extern String hostname;
+
+#endif
